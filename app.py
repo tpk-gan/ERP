c3ed4662309a4969008f3b3f9317b522c27aa6d2
tpk-gan <development@gantools.com>
2025-05-24 15:21:28 +0530

    ADDED A HOME PAGE AND PROPER USERS PAGE

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask_login import LoginManager, login_required, login_user, logout_user
+from flask_login import LoginManager, current_user, login_required, login_user, logout_user
 from models import DB, ROLE, USER
-from flask import Flask, redirect, render_template, request
+from flask import Flask, flash, redirect, render_template, request
 from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
@@ -42,7 +42,48 @@ def logout():
 @app.route('/')
 @login_required
 def home():
-    return render_template('base.html')
+    return render_template('index.html')
+
+@app.route('/users_and_roles', methods = ['GET', 'POST'])
+@login_required
+def users():
+
+    #prevent unauthorised access
+    if current_user.role.role_name != 'admin':
+        return "Access Denied", 403
+    
+    #post method to add new user
+    if request.method == 'POST':
+        #differentiate between add user and add role
+        if 'add_user_hidden_flag' in request.form:
+            #add new role
+            #take input from form
+            username = request.form['username_user_ip']
+            password = request.form['password_user_ip']
+            role_id = request.form['role_id_user_ip']
+
+            #check for repeat username
+            """pip install werkzeug bcrypt to get this working"""
+            if USER.query.filter_by(username = username).first():
+                flash('Username already exists')
+            else:
+                new_user = USER(
+                    username = username,
+                    password = generate_password_hash( password, method = "pbkdf2:sha256"),
+                    role_id = role_id
+                )
+
+                # add and commit the changes
+                DB.session.add(new_user)
+                DB.session.commit()
+                flash('User added successfully')
+        # (incomplete) add new user
+            
+    #get method to get list of usernames and roles
+    roles_app_var = ROLE.query.all()
+    users_app_var = USER.query.all()
+
+    return render_template('users_and_roles.html', users_html_var = users_app_var, roles_html_var = roles_app_var)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- setup.py
+++ setup.py
@@ -10,17 +10,25 @@ with app.app_context():
 
     # Create roles
     admin_role = ROLE(role_name='admin')
-    DB.session.add(admin_role)
+    user_role = ROLE(role_name='user')
+    DB.session.add_all([admin_role,user_role])
     DB.session.commit()
 
     # Create default admin user
     admin_user = USER(
         username='admin',
-        password=generate_password_hash('admin123'),
+        password=generate_password_hash('admin123',  method='pbkdf2:sha256'),
         role=admin_role
     )
-    DB.session.add(admin_user)
+
+    # Create default user user
+    user_user = USER(
+        username='user',
+        password=generate_password_hash('user456',  method='pbkdf2:sha256'),
+        role=user_role
+    )
+    DB.session.add_all([admin_user,user_user])
     DB.session.commit()
 
-    print("✅ Database initialized with roles and admin user.")
-    print("🔑 Login with username: admin, password: admin123")
+    print("✅ Database initialized with roles (admin,user) and (admin user , user user.)")
+    print("🔑 Login with username: admin/user, password: admin123/user456")
